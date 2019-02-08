fcf90a6ec20981bc9c1c4d6d43e2fe0b7944fc25
pascal <pascal.greim@rub.de>
2019-02-08 17:24:06 +0100

    Give buttons a basic functionality

---
+++ Assets/Scripts/ButtonFunctions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonFunctions : MonoBehaviour
+{
+	//code for starting a new game
+	public void newGameButton(){
+		Debug.Log("Starting new game... ");
+	}
+
+	//code for pressing the buttons on the gameboard
+	public void gameButton (int number){
+		Debug.Log("Button "+ number+ " is pressed");
+	}
+}
