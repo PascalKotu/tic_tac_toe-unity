bca81bc2d5ad7191ca7098360ab738c89c925918
pascal <pascal.greim@rub.de>
2019-02-08 19:55:05 +0100

    Disable clicked buttons

--- Assets/Scripts/ButtonFunctions.cs
+++ Assets/Scripts/ButtonFunctions.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class ButtonFunctions : MonoBehaviour
 {
+	[SerializeField]
+	private GameObject[] buttons;
+
 	private TicTacToe ttt= new TicTacToe();
+
 	//code for starting a new game
 	public void newGameButton(){
 		Debug.Log("Starting new game... ");
@@ -13,6 +17,7 @@ public class ButtonFunctions : MonoBehaviour
 	//code for pressing the buttons on the gameboard
 	public void gameButton (int number){
 		Debug.Log("Button "+ number+ " is pressed");
+		buttons[number].SetActive(false);
 		ttt.setArray(number);
 	}
 }
